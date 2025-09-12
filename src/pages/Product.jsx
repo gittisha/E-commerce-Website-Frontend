841b5b6d4c1f012ba9ed3c677630a0387215b135
gittisha <tishabanerjee124@gmail.com>
2025-09-12 13:48:21 +0530

    Description added

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 import Breadcrum from "../components/Breadcrums/Breadcrum";
 import ProductDisplay from "../components/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../components/DescriptionBox/DescriptionBox";
 
 const Product = () => {
   const {all_product}= useContext(ShopContext)
@@ -12,6 +13,7 @@ const Product = () => {
     <div>
       <Breadcrum product={product}/>
       <ProductDisplay product={product}/>
+      <DescriptionBox/>
     </div>
   )
    
